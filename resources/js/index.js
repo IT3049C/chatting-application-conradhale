9f9ef768c8d0f217921af0fa809993ff6dbc71bd
Conrad Hale <halec5@mail.uc.edu>
2025-02-11 12:52:33 -0500

    index.js: Mock getting messages

---
+++ resources/js/index.js
@@ -0,0 +1,57 @@
+const nameInput = document.getElementById('my-name-input')
+const messageInput = document.getElementById('my-message')
+const sendButton = document.getElementById('send-button')
+const chatBox = document.getElementById('chat')
+
+/**
+ * @returns {Message[]}
+ */
+function fetchMessages() {
+  return [
+    {
+      id: 1,
+      text: 'Whats up!',
+      sender: 'Conrad Hale',
+      timestamp: Date.now()
+    },
+    {
+      id: 2,
+      text: 'My man!',
+      sender: 'George Washington',
+      timestamp: Date.now()
+    },
+    {
+      id: 1,
+      text: 'How\'s it going George?',
+      sender: 'Conrad Hale',
+      timestamp: Date.now()
+    },
+  ]
+}
+
+/**
+ * @param {Message} msg
+ */
+function displayMessage(msg) {
+  const wrapper = document.createElement('div')
+  const sender = msg.sender
+  wrapper.classList.add('messages', sender === nameInput.value ? 'mine' : 'yours')
+
+  const messageText = document.createElement('div')
+  messageText.classList.add('message')
+  messageText.innerText = msg.text ?? ''
+  wrapper.appendChild(messageText)
+
+  const senderInfo = document.createElement('div')
+  senderInfo.classList.add('sender-info')
+  const time = new Date(msg.timestamp)
+  senderInfo.innerText = `${sender} ${time}`
+  wrapper.appendChild(senderInfo)
+  chatBox.appendChild(wrapper)
+}
+
+function updateMessages() {
+  fetchMessages().forEach(displayMessage)
+}
+
+updateMessages()
